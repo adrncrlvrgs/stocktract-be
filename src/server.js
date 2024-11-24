5367ee7b2184f03c74b1387778fab06563fadca6
adrncrl <adriancarl@qualitytrade.com>
2024-11-24 22:41:09 +0800

    issue: import error

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import router from 'routes';
+import 'module-alias/register.js';
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import router from './routes/index.js'; 
+
 
 dotenv.config();
 
@@ -11,8 +13,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
-app.use('/api', router);
+app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
