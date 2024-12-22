fc8ba5b416175dabea5a1ae933646bf43d017c7f
adrncrlvrgs <imadrianvargas@gmail.com>
2024-12-22 17:08:21 +0800

    add initial routes for items

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,15 @@ import express from 'express';
 import authRoutes from './authRoutes.js';
 import userRoutes from './userRoutes.js'
 import categoryRoutes from "./categoryRoutes.js"
+import itemRoutes from './itemsRoutes.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes); 
 router.use('/users', userRoutes)
 router.use('/category', categoryRoutes);
+router.use('/items', itemRoutes);
+
+
 
 export default router;
